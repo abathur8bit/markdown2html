7db951e8454f48ce82b9be809480c10f386dd645
abathur8bit <orion7019@gmail.com>
2026-04-13 18:20:44 -0500

    - guthub action for building release

--- lib/version.dart
+++ lib/version.dart
@@ -1,6 +1,6 @@
 /// GENERATED FILE - DO NOT EDIT
-const String appVersion = "1.04.00+0017";
+const String appVersion = "1.4.0";
 const int appVersionNumber = 1;
 const int appRevision = 4;
 const int appPatch = 0;
-const int appBuild = 17;
+const int appBuild = 18;
